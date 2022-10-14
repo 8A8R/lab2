0cdc748fa7baae4e1a66f3820b059ee723f80b1b
Ahmed88r <ahmed.haj8r@mail.ru>
2022-10-15 00:04:13 +0300

    lab 2

---
+++ main.py
@@ -0,0 +1,93 @@
+import csv
+
+count1 = 0
+count2 = 0
+a = 0
+
+def esc(code):
+    return f'\u001b[{code}m'
+
+
+def flag_cz():
+    for i in range(3):
+        print(RED + '  ' * (20) + END)
+
+    for i in range(3):
+        print(BLACK + '  ' * (20) + END)
+
+    for i in range(3):
+        print(WHITE + '  ' * (20) + END)
+
+
+def usor():
+    print(BLACK + '  ' * (1) + WHITE + '  ' * (3) + BLACK + '  ' * (1) + WHITE + '  ' * (3) + BLACK + '  ' * (
+        1) + WHITE + '  ' * (3) + BLACK + '  ' * (1) + END)
+    print(WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (
+        1) + BLACK + '  ' * (1) + WHITE + '  ' * (1)
+          + BLACK + '  ' * (1) + WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (1) + BLACK + '  ' * (
+              1) + WHITE + '  ' * (1) + END)
+    print(WHITE + '  ' * (2) + BLACK + '  ' * (1) + WHITE + '  ' * (3) + BLACK + '  ' * (1) + WHITE + '  ' * (
+        3) + BLACK + '  ' * (
+              1) + WHITE + '  ' * (2) + END)
+    print(WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (
+        1) + BLACK + '  ' * (1) + WHITE + '  ' * (1)
+          + BLACK + '  ' * (1) + WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (1) + BLACK + '  ' * (
+              1) + WHITE + '  ' * (1) + END)
+    print(BLACK + '  ' * (1) + WHITE + '  ' * (3) + BLACK + '  ' * (1) + WHITE + '  ' * (3) + BLACK + '  ' * (
+        1) + WHITE + '  ' * (3) + BLACK + '  ' * (1) + END)
+
+
+def func():
+    print(END)
+    print(18, WHITE + '  ' * (8) + BLACK + '  ' * (1) + END)
+    print(17, WHITE + '  ' * (8) + BLACK + '  ' * (1) + END)
+    print(16, WHITE + '  ' * (7) + BLACK + '  ' * (1) + WHITE + '  ' * (1) + END)
+    print(15, WHITE + '  ' * (7) + BLACK + '  ' * (1) + WHITE + '  ' * (1) + END)
+    print(14, WHITE + '  ' * (6) + BLACK + '  ' * (1) + WHITE + '  ' * (2) + END)
+    print(13, WHITE + '  ' * (6) + BLACK + '  ' * (1) + WHITE + '  ' * (2) + END)
+    print(12, WHITE + '  ' * (5) + BLACK + '  ' * (1) + WHITE + '  ' * (3) + END)
+    print(11, WHITE + '  ' * (5) + BLACK + '  ' * (1) + WHITE + '  ' * (3) + END)
+    print(10, WHITE + '  ' * (4) + BLACK + '  ' * (1) + WHITE + '  ' * (4) + END)
+    print(9, '', WHITE + '  ' * (4) + BLACK + '  ' * (1) + WHITE + '  ' * (4) + END)
+    print(8, '', WHITE + '  ' * (3) + BLACK + '  ' * (1) + WHITE + '  ' * (5) + END)
+    print(7, '', WHITE + '  ' * (3) + BLACK + '  ' * (1) + WHITE + '  ' * (5) + END)
+    print(6, '', WHITE + '  ' * (2) + BLACK + '  ' * (1) + WHITE + '  ' * (6) + END)
+    print(5, '', WHITE + '  ' * (2) + BLACK + '  ' * (1) + WHITE + '  ' * (6) + END)
+    print(4, '', WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (7) + END)
+    print(3, '', WHITE + '  ' * (1) + BLACK + '  ' * (1) + WHITE + '  ' * (7) + END)
+    print(2, '', BLACK + '  ' * (1) + WHITE + '  ' * (8) + END)
+    print(1, '', BLACK + '  ' * (1) + WHITE + '  ' * (8) + END)
+
+RED = esc(41)
+BLUE = esc(44)
+WHITE = esc(40)
+END = esc(0)
+BLACK = esc(107)
+
+flag_cz()
+usor()
+print('', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+func()
+
+with open('books-en.csv', 'r', encoding='cp1251') as csvfile:
+    table = csv.reader(csvfile, delimiter=';')
+    for row in table:
+        if a != 0:
+            if row[5] <= '2016':
+                count1 += 1
+            else:
+                count2 += 1
+        a += 1
+print(count1)
+print(count2)
+
+per1 = int((count1/(count1+count2))*100)
+print(per1)
+per2 = 100 - per1
+print(per2)
+print(f'   До 2018 {BLACK}{"  " * per1}{END}{per1}%')
+print(f'После 2016 {WHITE}{"  " * per2}{END}{"  " *(per1-per2)}{per2}%')
+
+
+
+
